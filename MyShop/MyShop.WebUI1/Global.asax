04e83423c43c13d35a01936461f1c4743f7d8088
Perdorues <Perdorues@DESKTOP-31A96OJ>
2023-05-17 15:27:36 +0200

    Initial Solution Setup

---
+++ MyShop/MyShop.WebUI1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyShop.WebUI1.MvcApplication" Language="C#" %>
---
+++ MyShop/MyShop.WebUI1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.WebUI1.Startup))]
+namespace MyShop.WebUI1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
